02bad4f64635e0bfd0f23968e076dd93ff16dda3
bart <bartolomej.kozorog@gmail.com>
2019-07-19 20:16:20 +0200

    Added classes parsing method getClasses

--- api-service/scraper/scraper.js
+++ api-service/scraper/scraper.js
@@ -1,12 +1,13 @@
-const {request, getHtml, getValue, getText} = require('./tools');
+const {getElements, getHtml, getValue, getText} = require('./tools');
 
-const RAZRED_SELECTOR_ID = 'id_parameter';
 const URNIKI_URL = 'https://www.easistent.com/urniki/';
 const SCNG_ID = 'e29aeb36cd1efde89c2b2c28e33209813ec32756';
 
 
 async function getClasses(html) {
+  const RAZRED_SELECTOR_ID = 'id_parameter';
   const MATCHES_WHITESPACE = /([ ]{2,}(\r\n|\r|\n))|[ ]{2,}/;
+
   let classes = getHtml(`#${RAZRED_SELECTOR_ID}`, html);
   return classes.split(MATCHES_WHITESPACE)
     .filter(ele => ele !== '\n' && ele !== undefined && ele !== '')
