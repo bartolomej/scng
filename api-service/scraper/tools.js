74f0a6dda4ee66cceeaf5a39f3743fec22f4df45
bart <bartolomej.kozorog@gmail.com>
2019-07-19 02:02:36 +0200

    Added basic scraping tools

---
+++ api-service/scraper/tools.js
@@ -0,0 +1,51 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs');
+
+
+module.exports.request = async function (url) {
+  return new Promise((resolve, reject) => {
+    axios.get(url).then(res => resolve(res.data)).catch(reject);
+  })
+};
+
+module.exports.getText = function (element, html) {
+  const $ = cheerio.load(html);
+  const elements = $(element);
+  let values = [];
+  for (let i = 0; i < elements.length; i++) {
+    for (let j = 0; j < elements[i].children.length; j++) {
+      if (elements[i].children[j].type === 'text') {
+        values.push(elements[i].children[j].data);
+      }
+    }
+  }
+  return values;
+};
+
+module.exports.getValue = function (element, html) {
+  const $ = cheerio.load(html);
+  const elements = $(element);
+  let values = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].attribs.value !== undefined) {
+      values.push(elements[i].attribs.value)
+    }
+  }
+  return values;
+};
+
+module.exports.getHtml = function (element, html) {
+  const $ = cheerio.load(html);
+  return $(element).html();
+};
+
+module.exports.readFile = async function (filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.join(__dirname, filePath),
+      {encoding: 'utf-8'}, (err, data) => {
+      err ? reject(err) : resolve(data)
+    })
+  })
+};
